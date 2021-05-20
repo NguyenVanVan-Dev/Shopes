a844535e31fd5ef4134c0730c259266a2285f7b9
Nguyenvanvan-yone <70500662+Nguyenvanvan-yone@users.noreply.github.com>
2021-05-20 15:54:18 +0700

    lam activity bag,filter,productbag

---
+++ app/src/main/java/com/example/shopbangiay/bag.java
@@ -0,0 +1,15 @@
+package com.example.shopbangiay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bag);
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/shopbangiay/filter.java
@@ -0,0 +1,15 @@
+package com.example.shopbangiay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class filter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        getSupportActionBar().hide();
+    }
+}
