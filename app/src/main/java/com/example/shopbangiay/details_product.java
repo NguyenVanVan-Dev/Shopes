7eddb70f4292e6e3bdcbdcbf5a7e54f9331d6bf7
Nguyenvanvan-yone <70500662+Nguyenvanvan-yone@users.noreply.github.com>
2021-05-20 10:03:33 +0700

    add file github

---
+++ app/src/main/java/com/example/shopbangiay/details_product.java
@@ -0,0 +1,15 @@
+package com.example.shopbangiay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class details_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_product);
+        getSupportActionBar().hide();
+    }
+}
